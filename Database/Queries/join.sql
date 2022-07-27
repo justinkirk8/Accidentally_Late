4504195216311b7ae8565ba319d62b5c4f363e4b
justinkirk8 <96632211+justinkirk8@users.noreply.github.com>
2022-07-27 11:17:15 -0400

    update to join and schema code and to gitignore

--- Database/Queries/join.sql
+++ Database/Queries/join.sql
@@ -1,9 +1,26 @@
--- reset
+-- reset merged
 DROP TABLE accidents_joined:
-
--- join
+-- join merged
 Select ac.severity, ac.state_code, us.region, us.division, ac.temperature_f, ac.visibility_mi, ac.wind_speed_mph, ac.precipitation_in, ac.weather_condition, ac.sunrise_sunset, ac.year,ac.date
 INTO accidents_joined
 From accidents_merged AS ac
+JOIN us_state_region as us
+ON(ac.state_code = us.state_code);
+
+-- reset pre-covid
+DROP TABLE accidents_pre_covid_joined:
+-- join pre-covid
+Select ac.severity, ac.state_code, us.region, us.division, ac.temperature_f, ac.visibility_mi, ac.wind_speed_mph, ac.precipitation_in, ac.weather_condition, ac.sunrise_sunset, ac.year,ac.date
+INTO accidents_pre_covid_joined
+From accidents_pre_covid AS ac
+JOIN us_state_region as us
+ON(ac.state_code = us.state_code);
+
+-- reset covid
+DROP TABLE accidents_covid_joined:
+-- join covid
+Select ac.severity, ac.state_code, us.region, us.division, ac.temperature_f, ac.visibility_mi, ac.wind_speed_mph, ac.precipitation_in, ac.weather_condition, ac.sunrise_sunset, ac.year,ac.date
+INTO accidents_covid_joined
+From accidents_pre_covid AS ac
 JOIN us_state_region as us
 ON(ac.state_code = us.state_code);
--- Database/Queries/schema.sql
+++ Database/Queries/schema.sql
@@ -1,16 +1,49 @@
 -- create accidents-merged table
 DROP TABLE accidents_merged;
 CREATE TABLE accidents_merged (
- 	Severity VARCHAR(40) NOT NULL,
-	State_code VARCHAR(40) NOT NULL,
-	Temperature_F NUMERIC NOT NULL,
-	Visibility_mi NUMERIC NOT NULL,
-	Wind_Speed_mph NUMERIC NOT NULL,
-	Precipitation_in NUMERIC NOT NULL,
-	Weather_Condition VARCHAR(40) NOT NULL,
-	Sunrise_Sunset VARCHAR(40) NOT NULL,
+ 	severity VARCHAR(40) NOT NULL,
+	state_code VARCHAR(40) NOT NULL,
+	temperature_f NUMERIC NOT NULL,
+	visibility_mi NUMERIC NOT NULL,
+	wind_speed_mph NUMERIC NOT NULL,
+	precipitation_in NUMERIC NOT NULL,
+	weather_condition VARCHAR(40) NOT NULL,
+	sunrise_sunset VARCHAR(40) NOT NULL,
 	year NUMERIC NOT NULL,
-	date DATE NOT NULL
+	date DATE NOT NULL,
+	classification VARCHAR(40) NOT NULL
+);
+
+-- create accidents-merged table
+DROP TABLE accidents_pre_covid;
+CREATE TABLE accidents_pre_covid (
+ 	severity VARCHAR(40) NOT NULL,
+	state_code VARCHAR(40) NOT NULL,
+	temperature_f NUMERIC NOT NULL,
+	visibility_mi NUMERIC NOT NULL,
+	wind_speed_mph NUMERIC NOT NULL,
+	precipitation_in NUMERIC NOT NULL,
+	weather_condition VARCHAR(40) NOT NULL,
+	sunrise_sunset VARCHAR(40) NOT NULL,
+	year NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	classification VARCHAR(40) NOT NULL
+);
+
+-- create accidents-merged table
+DROP TABLE accidents_covid;
+CREATE TABLE accidents_covid (
+ 	severity VARCHAR(40) NOT NULL,
+	state_code VARCHAR(40) NOT NULL,
+	temperature_f NUMERIC NOT NULL,
+	visibility_mi NUMERIC NOT NULL,
+	wind_speed_mph NUMERIC NOT NULL,
+	precipitation_in NUMERIC NOT NULL,
+	weather_condition VARCHAR(40) NOT NULL,
+	sunrise_sunset VARCHAR(40) NOT NULL,
+	year NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	classification VARCHAR(40) NOT NULL
 );
 
 -- create US_State_Region table
